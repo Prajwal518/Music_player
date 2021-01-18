75b3c12cfdbaa630c02e546e88659df8a45811f4
Prajwal518 <prajwalgowda.s518@gmail.com>
2021-01-18 20:33:55 +0530

    added Home,Login and register page and took out index.html and added it to index.ejs

--- app.js
+++ app.js
@@ -1,17 +1,70 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
-// const ejs = require("ejs");
-// const mongoose = require('mongoose');
+const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/MusicDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+  email:String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password == password){
+          res.render("index");
+        }else{
+          res.render("home");
+        }
+      }
+    }
+  });
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const newUser = new  User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("index");
+    }
+  });
 });
 
 app.listen(3000, function() {
